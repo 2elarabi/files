d6758da079fead1974ca054865d5147227adb25b
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-01-24 13:27:11 +0100

    Update sprint_host_3.py

--- sprint_host_3.py
+++ sprint_host_3.py
@@ -194,7 +194,7 @@ def run_register(driver):
             try:
                 button = driver.find_element(
                     By.CSS_SELECTOR,
-                    "ui5-button-main ui5-button-main--grey ui5-button-main--L ui5-button-main--dark ui5-button-main--full"
+                    ".ui5-button-main.ui5-button-main--grey.ui5-button-main--L.ui5-button-main--dark.ui5-button-main--full"
                 )
                 button.click()
                 thus = False
