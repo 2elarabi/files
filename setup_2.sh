3153c8558e78c79f7f95725f04916013b67a3176
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-01-25 00:34:49 +0100

    Update setup_2.sh

--- setup_2.sh
+++ setup_2.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install -y python3-pip
-sudo pip3 install requests supabase selenium undetected_chromedriver faster-whisper
+#sudo pip3 install requests supabase selenium undetected_chromedriver faster-whisper whisper
+sudo pip3 install requests supabase selenium undetected_chromedriver whisper
 sudo wget https://raw.githubusercontent.com/2elarabi/files/main/sprint_host_3.py
 python3 sprint_host_3.py
