6003bde1f63c74ad2457e715f46d1426879fa78c
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-01-26 01:10:53 +0100

    Update setup_2.sh

--- setup_2.sh
+++ setup_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt update
+sudo wget https://raw.githubusercontent.com/2elarabi/files/main/sprint_host_3_captcha_open_ai_two_captcha.py
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install -y python3-pip
-sudo wget https://raw.githubusercontent.com/2elarabi/files/main/sprint_host_3_captcha_open_ai_two_captcha.py
 #sudo pip3 install requests supabase selenium undetected_chromedriver faster-whisper
 sudo pip3 install requests supabase selenium undetected_chromedriver whisper openai-whisper
 sudo pip3 install torch --index-url https://download.pytorch.org/whl/cu121
