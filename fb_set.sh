7efe9b5b8605134aad9e1b3b2499fd081bd852ff
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-02-08 13:10:16 +0100

    Update and rename firebase_setup.sh to fb_set.sh

--- fb_set.sh
+++ fb_set.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/config.json
+sudo apt install unzip
+sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/top_photo.zip
 sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/main_desktop_with_update.py
-sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/serviceAccountKey.json
+unzip -P ?$Arbi@Naji1987$! top_photo.zip
 sudo apt install -y python3-pip
 sudo pip3 install firebase-admin supabase requests google-auth google-auth-oauthlib google-auth-httplib2
 python3 main_desktop_with_update.py
