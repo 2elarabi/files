1ed0e046c2114c66a7ac58c02026a6a72bcebdf1
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-02-08 13:38:24 +0100

    Update fb_set.sh

--- fb_set.sh
+++ fb_set.sh
@@ -5,7 +5,7 @@ sudo apt install unzip
 sudo apt install p7zip-full
 sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/top_photo.zip
 sudo wget https://raw.githubusercontent.com/2elarabi/files/refs/heads/main/main_desktop_with_update.py
-7z x top_photo.zip ?$Arbi@Naji1987$!
+7z x top_photo.zip -p'?$Arbi@Naji1987$!'
 sudo apt install -y python3-pip
 sudo pip3 install firebase-admin supabase requests google-auth google-auth-oauthlib google-auth-httplib2
 python3 main_desktop_with_update.py
