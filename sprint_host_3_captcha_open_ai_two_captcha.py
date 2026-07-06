9b6cf15f3ccef395d4bb4f67894fd2c0fa19d9a5
2elarabi <122154600+2elarabi@users.noreply.github.com>
2026-07-06 14:58:57 +0100

    Update sprint_host_3_captcha_open_ai_two_captcha.py

--- sprint_host_3_captcha_open_ai_two_captcha.py
+++ sprint_host_3_captcha_open_ai_two_captcha.py
@@ -42,6 +42,8 @@ python sprint_host_3_captcha_open_ai.py
 """
 options = ChromeOptions()
 
+proxy = "31.56.127.193:7684:khjiupdw:cvn4jdjbprnv"
+options.add_argument(f"--proxy-server=http://{proxy}")
 
 def get_alias_id_by_email(zone_id, email):
     r = requests.get(
